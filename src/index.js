a58167fe24eb20213ed5d697700901dc51b521da
camilothomas <cembre@gmail.com>
2019-05-08 00:32:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+
+
+class Interest extends React.Component {
+
+
+
+
+  render() {
+    return(
+    	<div>
+    	Where's the div
+    	</div>
+    );
+  }
+
+}
+
+// ------------
+
+ReactDOM.render(
+  <Interest />,
+  document.getElementById('root')
+);
